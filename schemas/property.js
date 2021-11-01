94f8ddff1d7a5b812121927a5599422176ca6929
muchirijane <muchirijane@outlook.com>
2021-11-01 11:41:32 +0300

    init sanity

---
+++ schemas/property.js
@@ -0,0 +1,12 @@
+export default {
+  name: "property",
+  title: "property",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+  ],
+};
